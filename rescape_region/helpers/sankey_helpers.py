7c4fee44d77598bb5ea1e1ebf04deb2fdb4adde1
Andy Likuski <andy@likuski.org>
2019-10-07 11:27:21 -0700

    Hadling explicit sankey links

--- rescape_region/helpers/sankey_helpers.py
+++ rescape_region/helpers/sankey_helpers.py
@@ -1,6 +1,7 @@
 from rescape_python_helpers import ramda as R
 import locale
 import logging
+
 logger = logging.getLogger('rescape_region')
 
 
@@ -20,6 +21,10 @@ def string_to_float(flt):
 
 def stages_by_name(stages):
     return R.map_prop_value_as_index('name', stages)
+
+
+def stages_by_key(stages):
+    return R.map_prop_value_as_index('key', stages)
 
 
 def aberrate_location(index, location, factor=.005):
@@ -59,6 +64,29 @@ def create_raw_nodes(delineator, resource):
     )
 
 
+def create_raw_links(delineator, resource):
+    """
+        Creates links from the csv if present
+    :param resource: The Resource object
+    :return: Raw node data
+    """
+    columns = R.item_path(['data', 'settings', 'link_columns'], resource)
+    raw_data = R.item_path(['data', 'rawData'], resource)
+    # Sometimes we split nodes and edges in the raw data into
+    # dict(nodes=..., edges=...). Sometimes the raw data is just nodes
+    # and we get the edges from the node data
+    raw_links = R.prop_or(None, 'links', raw_data)
+    return R.map(
+        lambda line: R.from_pairs(
+            zip(
+                columns,
+                R.map(lambda s: s.strip(), line.split(delineator))
+            )
+        ),
+        raw_links
+    )
+
+
 def resolve_coordinates(default_location, coordinates, i):
     """
         Resolves the lat/lon based on the given coordinates string. If it is NA then default to BRUSSELS_LOCATION
@@ -70,7 +98,7 @@ def resolve_coordinates(default_location, coordinates, i):
 
     # TODO instead of aberrating we should create a visualizatoin to show things that are in the same place
     # Something like a number icon that spreads when you mouse over
-    if coordinates == 'NA':
+    if not coordinates or coordinates == 'NA':
         return dict(
             isGeneralized=True,
             location=aberrate_location(i, default_location)
@@ -80,6 +108,10 @@ def resolve_coordinates(default_location, coordinates, i):
             isGeneralized=False,
             location=list(reversed(R.map(lambda coord: string_to_float(coord), coordinates.split(','))))
         )
+
+
+def prop_lookup(node, prop):
+    return R.prop(prop, dict(zip(node['properties'], node['propertyValues'])))
 
 
 def create_links(stages, value_key, nodes_by_stages):
@@ -114,9 +146,6 @@ def create_links(stages, value_key, nodes_by_stages):
             return []
         targets = nodes_by_stages[R.prop('key', target_stage)]
 
-        def prop_lookup(node, prop):
-            return R.prop(prop, dict(zip(node['properties'], node['propertyValues'])))
-
         # Create the link with the source_node and target_node. Later we'll add
         # in source and target that points to the nodes overall index in the graph,
         # but we don't want to compute the overall indices yet
@@ -148,10 +177,16 @@ def generate_sankey_data(resource):
     value_key = R.prop('valueKey', settings)
     location_key = R.prop('locationKey', settings)
     node_name_key = R.prop('nodeNameKey', settings)
+    node_color_key = R.prop_or(None, 'nodeColorKey', settings)
     default_location = R.prop('defaultLocation', settings)
     delineator = R.prop_or(';', 'delineator', settings)
     # A dct of stages by name
     stage_by_name = stages_by_name(stages)
+
+    link_start_node_key = R.prop_or(None, 'linkStartNodeKey', settings)
+    link_end_node_key = R.prop_or(None, 'linkEndNodeKey', settings)
+    link_value_key = R.prop_or(None, 'linkValueKey', settings)
+    link_color_key = R.prop_or(None, 'linkColorKey', settings)
 
     def accumulate_nodes(accum, raw_node, i):
         """
@@ -162,11 +197,13 @@ def generate_sankey_data(resource):
         :param i:
         :return:
         """
-        location_obj = resolve_coordinates(default_location, R.prop(location_key, raw_node), i)
+        location_obj = resolve_coordinates(default_location, R.prop_or(None, location_key, raw_node), i)
         location = R.prop('location', location_obj)
         is_generalized = R.prop('isGeneralized', location_obj)
         # The key where then node is stored is the stage key
-        key = R.prop('key', stage_by_name[raw_node[stage_key]])
+        node_stage = raw_node[stage_key]
+        # Get key from name or it's already a key
+        key = R.prop('key', R.prop_or(dict(key=node_stage), node_stage, stage_by_name))
 
         # Copy all properties from resource.data  except settings and raw_data
         # Also grab raw_node properties
@@ -203,6 +240,7 @@ def generate_sankey_data(resource):
         )
 
     raw_nodes = create_raw_nodes(delineator, resource)
+
     # Reduce the nodes
     nodes_by_stage = R.reduce(
         lambda accum, i_and_node: accumulate_nodes(accum, i_and_node[1], i_and_node[0]),
@@ -210,10 +248,36 @@ def generate_sankey_data(resource):
         enumerate(raw_nodes)
     )
     nodes = R.flatten(R.values(nodes_by_stage))
+    try:
+        has_links = True
+        # Throw if link_columns aren't defined. This is expected for sankeys without explicit links
+        R.item_path(['data', 'settings', 'link_columns'], resource)
+    except:
+        has_links = False
+    if has_links:
+        raw_links = create_raw_links(delineator, resource)
+        node_key_key = R.prop('nodeKeyKey', settings)
+        nodes_by_key = R.from_pairs(R.map(
+            lambda node: [prop_lookup(node, node_key_key), node],
+            nodes
+        ))
+        links = R.map(
+            lambda link: dict(
+                source_node=nodes_by_key[link[link_start_node_key]],
+                target_node=nodes_by_key[link[link_end_node_key]],
+                value=link[link_value_key],
+                color=R.prop_or(None, link_color_key, link)
+            ),
+            raw_links
+        )
+    else:
+        # Guess links from nodes and stages
+        links = create_links(stages, value_key, nodes_by_stage)
     return dict(
         nodes=nodes,
         nodes_by_stage=nodes_by_stage,
-        links=create_links(stages, value_key, nodes_by_stage)
+        # We might have explicit links or have to generate all possible based on the nodes
+        links=links
     )
 
 
