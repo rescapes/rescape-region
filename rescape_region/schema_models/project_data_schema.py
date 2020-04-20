5b6b0d6faa4d838d9fc5d0cdbb64745a08fedd54
Andy Likuski <andy@likuski.org>
2020-04-20 15:23:19 +0200

    Project data location params

--- rescape_region/schema_models/project_data_schema.py
+++ rescape_region/schema_models/project_data_schema.py
@@ -1,15 +1,15 @@
-from rescape_graphene import resolver_for_dict_field, type_modify_fields, resolver_for_dict_list
-from graphene import ObjectType, Field, List
+from graphene import ObjectType, Field
+from rescape_graphene import resolver_for_dict_field, type_modify_fields
 
-from rescape_region.model_helpers import get_location_schema
+from rescape_region.schema_models.location_params_data_schema import LocationParamsDataType, location_params_data_fields
 from rescape_region.schema_models.user_state_data_schema import MapboxDataType, mapbox_data_fields
 
 project_data_fields = dict(
     # This is a singular object with a params field used to resolve locations
     locations=dict(
-        type=get_location_schema()['graphene_class'],
-        graphene_type=get_location_schema()['graphene_class'],
-        fields=get_location_schema()['graphene_fields'],
+        type=LocationParamsDataType,
+        graphene_type=LocationParamsDataType,
+        fields=location_params_data_fields,
         type_modifier=lambda *type_and_args: Field(*type_and_args, resolver=resolver_for_dict_field)
     ),
 
