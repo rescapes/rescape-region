78331b2912ec542848661b3b0bec676bb80b78cb
Andy Likuski <andy@likuski.org>
2021-05-10 09:21:26 +0200

    Added additional_user_scope_schemas to user state so applications can define their own schemas within userRegions and userProjects

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ class CleanCommand(setuptools.Command):
 
 setuptools.setup(
     name="rescape-region",
-    version="0.2.8",
+    version="0.2.9",
     author="Andy Likuski",
     author_email="andy@likuski.org",
     description="Geospatial region support for Rescape projects",
