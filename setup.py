9488b9881bde798e20d4856e83c71e026a3dc036
Andy Likuski <andy@likuski.org>
2021-12-20 09:34:46 +0100

    Version update

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ class CleanCommand(setuptools.Command):
 
 setuptools.setup(
     name="rescape-region",
-    version="0.2.30",
+    version="0.2.31",
     author="Andy Likuski",
     author_email="andy@likuski.org",
     description="Geospatial region support for Rescape projects",
